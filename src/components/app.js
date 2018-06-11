d66545d7cf92312b15b5cd5a5bc9813d64e3d4aa
samskarvan <samskarvan@gmail.com>
2018-06-11 07:07:02 -0700

    footer styling

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/css/app.css';
 import ContactForm from './contactForm';
 import Header from './header';
+import Footer from './footer';
 import Home from './home';
 
 const App = () => (
     <div>
-        <div className="app"> 
+        <div className="container-fluid"> 
         <Header/>
-        <div className="container content">
+        <div className="content">
             <Route exact path="/" component={Home}/>
             <Route path="/contact" component={ContactForm}/>
-            </div>
+        </div>
+        <Footer/>
         </div>
     </div>
 );
